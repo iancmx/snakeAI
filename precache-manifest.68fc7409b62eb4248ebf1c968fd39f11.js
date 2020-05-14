b1aa3c3323b53c5157fe13c4378dec2d68627aed
Ian Choy <iancmx@hotmail.com>
2020-05-14 17:49:10 -0600

    Updates

---
+++ precache-manifest.68fc7409b62eb4248ebf1c968fd39f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bbadf17254dc21951549d785b2cae9",
+    "url": "/snakeAI/index.html"
+  },
+  {
+    "revision": "637612da1640ebbdde42",
+    "url": "/snakeAI/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "21541d1bcc2832af9a92",
+    "url": "/snakeAI/static/css/main.902490b7.chunk.css"
+  },
+  {
+    "revision": "637612da1640ebbdde42",
+    "url": "/snakeAI/static/js/2.561dfdd5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/snakeAI/static/js/2.561dfdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21541d1bcc2832af9a92",
+    "url": "/snakeAI/static/js/main.9fd5d964.chunk.js"
+  },
+  {
+    "revision": "2686a32751f7ee6876bd",
+    "url": "/snakeAI/static/js/runtime-main.925a331e.js"
+  }
+]);
